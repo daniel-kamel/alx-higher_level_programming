568299e0425d175f6d8b910210a79fb04abc8025
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-01-04 17:49:20 +0200

    Create 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 
 
 def main():
     a = 1
     b = 2
 
-    print(f"{a} + {b} = {add_0.add(a, b)}")
+    print(f"{a} + {b} = {add(a, b)}")
 
 
 if __name__ == "__main__":
