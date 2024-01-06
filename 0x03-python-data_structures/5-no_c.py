0206dd20142f3b108a6c9a1d14611b5888eddd6f
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-01-06 18:35:51 +0200

    Create 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,7 +5,7 @@ def no_c(my_string):
     i = 0
 
     while i < len(my_string):
-        if my_string[i] in ['c', 'C']:
+        if my_string[i] in 'cC':
             my_string = my_string[:i] + my_string[i + 1:]
 
         i += 1
