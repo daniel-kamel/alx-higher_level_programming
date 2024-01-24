0c3d6af12c44ae13ba2db4c1e614d4911f7e06c8
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-01-24 03:49:02 +0200

    Create 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,44 +4,53 @@
 
 class Square:
     """
-    Class that defines properties of square by: (based on 5-square.py).
+   Class that defines properties of square by: (based on 0-square.py)
 
     Attributes:
-        size: size of a square (1 side).
+        size: size of a square (1 side)
+        position: position of the square
     """
     def __init__(self, size=0, position=(0, 0)):
-        """Creates new instances of square.
+        """
+        Creates new instances of square.
 
         Args:
-            __size (int): size of the square (1 side).
-            __position (tuple): position of the square.
+            __size (int): size of the square (1 side)
+            __position (tuple): position of the square
         """
         self.size = size
         self.position = position
 
     def area(self):
-        """Calculates the area of square.
+        """
+        Calculates the area of square
 
-        Returns: the current square area.
+        Returns:
+            The area of the square insatnce
         """
         return self.__size ** 2
 
     @property
     def size(self):
-        """Returns the size of a square
+        """
+        Getter method for the size field
+
+        Return:
+            size of the square instance
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Property setter for size.
+        """
+        Setter method for the size field
 
         Args:
-            value (int): size of a square (1 side).
+            size: size of the sqaure
 
         Raises:
-            TypeError: size must be an integer.
-            ValueError: size must be >= 0.
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -51,13 +60,18 @@ class Square:
 
     @property
     def position(self):
-        """Returns the position of the square
+        """
+        Getter method for the position field
+
+        Return:
+            position of the square instance
         """
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Property setter for position.
+        """
+        Setter method for the position field
 
         Args:
             value (tuple): position of the square.
@@ -76,7 +90,8 @@ class Square:
         self.__position = value
 
     def my_print(self):
-        """prints in stdout the square with the character #
+        """
+        prints a square to stdout
         """
 
         if self.__size == 0:
