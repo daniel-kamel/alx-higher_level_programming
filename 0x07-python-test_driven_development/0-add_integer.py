429c6bf64d17808392488434fa399d4c69b1a262
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-01-28 20:31:07 +0200

    Create 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contains a function that adds 2 numbers
+
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds 2 numbers.
+    
+    Args:
+        a (int or float): first number
+        b (int or float): second number (default = 98)
+        
+    Returns:
+        int: The sum of a and b
+    
+    Raises:
+        TypeError: a and b must be either int or float
+    """
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+    
+    return int(a + b)
