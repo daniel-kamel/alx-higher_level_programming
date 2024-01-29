f39b89071b03774bbf4a75c40eba613bd197a574
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-01-29 21:24:49 +0200

    Create 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,15 +9,24 @@ class Rectangle():
     This class defines an empty Rectangle
     """
     def __init__(self, width=0, height=0):
+        """
+        Initializes instances or Rectangle class
+        """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+        """
+        Gets the width property
+        """
         return self.__width
 
     @width.setter
     def width(self, width):
+        """
+        Sets the width property
+        """
         if type(width) is not int:
             raise TypeError('width must be an integer')
         if width < 0:
@@ -27,10 +36,16 @@ class Rectangle():
 
     @property
     def height(self):
+        """
+        Gets the height property
+        """
         return self.__height
 
     @height.setter
     def height(self, height):
+        """
+        Sets the height property
+        """
         if type(height) is not int:
             raise TypeError('height must be an integer')
         if height < 0:
