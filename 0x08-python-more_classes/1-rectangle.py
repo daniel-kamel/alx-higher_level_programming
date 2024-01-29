5dc8beb89271781d20d2bfff7b34c209c5150d00
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-01-29 21:33:40 +0200

    Create 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,10 +7,18 @@ This module contains the definition of a Rectangle class
 class Rectangle():
     """
     This class defines an empty Rectangle
+
+    Attributes:
+        width (int): width of the rectangle
+        height (int): height of the rectangle
     """
     def __init__(self, width=0, height=0):
         """
         Initializes instances or Rectangle class
+
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
         """
         self.__width = width
         self.__height = height
@@ -19,6 +27,9 @@ class Rectangle():
     def width(self):
         """
         Gets the width property
+
+        Returns:
+            The width of the rectangle
         """
         return self.__width
 
@@ -26,6 +37,13 @@ class Rectangle():
     def width(self, width):
         """
         Sets the width property
+
+        Args:
+            width (int): width of the rectangle
+
+        Raises:
+            TypeError: width must be an integer
+            ValueError: width must be >= 0
         """
         if type(width) is not int:
             raise TypeError('width must be an integer')
@@ -38,6 +56,9 @@ class Rectangle():
     def height(self):
         """
         Gets the height property
+
+        Returns:
+            The height of the rectangle
         """
         return self.__height
 
@@ -45,6 +66,13 @@ class Rectangle():
     def height(self, height):
         """
         Sets the height property
+
+        Args:
+            height (int): height of the rectangle
+
+        Raises:
+            TypeError: height must be an integer
+            ValueError: height must be >= 0
         """
         if type(height) is not int:
             raise TypeError('height must be an integer')
