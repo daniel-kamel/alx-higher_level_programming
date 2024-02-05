187a03a5d6ccd60e22072e577aa4ac0676c12887
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-02-05 19:43:33 +0200

    Create 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+is same class module doc
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    returns if the objects is an instance of the specified class
+    """
+    return type(obj) is a_class
