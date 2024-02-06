c72d85be5e9084c4658b3e68db1e2012da2b004a
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-02-06 12:06:52 +0200

    Create 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ def read_file(filename=""):
     '''
     read_file function doc
     '''
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         print(f.read())
