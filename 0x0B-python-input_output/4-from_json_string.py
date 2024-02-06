1f3582d8e2f6d50cfe438f7ca106fe747b8eb33e
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-02-06 11:56:44 +0200

    Create 4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+4-from_json_string module doc
+'''
+import json
+
+
+def from_json_string(my_str):
+    '''
+    from_json_string function doc
+    '''
+    return json.loads(my_str)
