6d20490eabb2c65879bde20c30779590b3450b72
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-02-06 11:54:34 +0200

    Create 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+3-to_json_string module doc
+'''
+import json
+
+
+def to_json_string(my_obj):
+    '''
+    to_json_string function doc
+    '''
+    return json.dumps(my_obj)
