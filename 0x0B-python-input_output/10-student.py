88cb40b8f353ac699abd1f4dbce0766638c8fe34
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-02-07 02:12:22 +0200

    Create 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+9-student module doc
+'''
+
+
+class Student:
+    '''
+    Student class doc
+    '''
+    def __init__(self, first_name, last_name, age) -> None:
+        '''
+        initializes Student object
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        returns object attributes as json-like dictionary
+        '''
+        items = {}
+        for key, value in vars(self).items():
+            if key[:2] != '__' and key[-2:] != '__':
+                if attrs:
+                    if key in attrs:
+                        items[key] = value
+                else:
+                    items[key] = value
+        return items
