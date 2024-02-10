c4402f5ab9087498effaecdb2ef294af1350dbb4
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-02-10 18:11:33 +0200

    Create models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -128,3 +128,9 @@ class Rectangle(Base):
         '''
         for _ in range(self.__height):
             print('#' * self.__width, )
+
+    def __str__(self) -> str:
+        return '[Rectangle] ({}) {}/{} - {}/{}'.format(
+            self.id, self.__x, self.__y,
+            self.__width, self.__height
+        )
