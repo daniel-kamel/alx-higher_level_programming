93c4975ffaa3d598415393e5a24e25c3f357a732
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-02-11 16:54:45 +0200

    Create models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 '''
 Base module containing Base class
 '''
+import json
 
 
 class Base:
@@ -19,3 +20,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''
+        returns json string representation of list_dictionaries
+        '''
+        if list_dictionaries is None:
+            return '[]'
+        else:
+            return json.dumps(list_dictionaries)
