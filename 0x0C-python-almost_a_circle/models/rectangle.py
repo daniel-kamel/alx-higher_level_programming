3c99c2b9c13c5c2c1156af41b1ae887fc6866c23
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-02-11 14:30:43 +0200

    Create models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -136,3 +136,19 @@ class Rectangle(Base):
             self.id, self.__x, self.__y,
             self.__width, self.__height
         )
+
+    def update(self, *args):
+        '''
+        updates Rectangle attributes
+        '''
+        for i in range(len(args)):
+            if i == 0:
+                self.id = args[i]
+            elif i == 1:
+                self.__width = args[i]
+            elif i == 2:
+                self.__height = args[i]
+            elif i == 3:
+                self.__x = args[i]
+            elif i == 4:
+                self.__y = args[i]
