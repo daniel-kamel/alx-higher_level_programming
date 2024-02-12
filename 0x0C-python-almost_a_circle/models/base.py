9b85f8fd9fa36ad58066aa3178f95544a9dc8313
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-02-12 17:31:36 +0200

    Create models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,3 +41,12 @@ class Base:
 
         with open(f'{cls.__name__}.json', 'w') as f:
             f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''
+        returns the list of the JSON string representation json_string
+        '''
+        if json_string is None:
+            return []
+        return json.loads(json_string)
