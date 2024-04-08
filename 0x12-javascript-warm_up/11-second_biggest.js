6fb95d2dd6d957d36ea41a6b15dec2f37c35a254
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-08 22:33:30 +0200

    Create 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,22 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length < 4) {
-  console.log('0');
-} else {
-  const list = argv.sort();
-  console.log(list.reverse()[2]);
+function getList (argv) {
+  const list = [];
+  if (argv.length < 4) {
+    return 0;
+  }
+  for (let i = 2; i < argv.length; i++) {
+    list.push(Number(argv[i]));
+  }
+  return list;
 }
+
+function secondBiggest (list) {
+  if (list === 0) {
+    return 0;
+  }
+  list.sort(function (a, b) { return a - b; });
+  return list.reverse()[1];
+}
+
+console.log(secondBiggest(getList(argv)));
