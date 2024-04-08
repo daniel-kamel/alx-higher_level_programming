f860c96a2f94374d5438c525658578426942e708
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-08 21:22:49 +0200

    Create 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length <= 3) {
-  console.log(0);
+if (argv.length < 4) {
+  console.log('0');
 } else {
   const list = argv.sort();
   console.log(list.reverse()[2]);
