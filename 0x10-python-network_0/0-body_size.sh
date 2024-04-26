a7ef5aaedd21738a23c20d7e3f7e94af8bff0764
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-26 22:47:01 +0300

    Create 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <url>"
-  exit 1
-fi
-
-curl -s "$1" | wc -c
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
