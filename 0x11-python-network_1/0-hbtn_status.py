1a74ec2e46c59c9a2ae50975e6d2d80f0bf795e9
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-27 06:16:06 +0300

    Create 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""displays the body of the response"""
+from urllib import request
+
+
+def main():
+  """displays the body of the response"""
+  with request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
+
+
+if __name__ == "__main__":
+  main()
