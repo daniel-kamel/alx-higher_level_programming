41485d405d102ef5dabb15d26b36ea73bed1ddda
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-27 19:38:01 +0300

    Create 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+import requests
+from sys import argv
+
+
+def main():
+    """Sends a POST request to the passed URL with the email as a parameter"""
+    url = argv[1]
+    data = {'q': argv[2]}
+    r = requests.post(url, data=data)
+    try:
+        js = r.json()
+        if len(js) == 0:
+            print("No result")
+        else:
+            print('[{}] {}'.format(js.get('id'), js.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    main()
