eb99820b0eafdd21a3ec32504699eb50765a7570
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-27 06:22:41 +0300

    fix indentation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,14 +4,14 @@ from urllib import request
 
 
 def main():
-  """displays the body of the response"""
-  with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    """displays the body of the response"""
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
 
 
 if __name__ == "__main__":
-  main()
+    main()
