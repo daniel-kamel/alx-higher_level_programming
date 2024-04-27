41503883241a428f7151c31ebc7e692730ca0567
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-27 19:26:21 +0300

    Create 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Displays the body of the response and handles error codes"""
+import requests
+from sys import argv
+
+
+def main():
+    """displays the body of the response"""
+    url = argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print('Error code: {}'.format(r.status_code))
+    else:
+        print(r.text)
