fd95803aef7269e14ecd0ca1d14042dea836227e
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-27 03:52:43 +0300

    Create 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,18 +7,5 @@ def find_peak(list_of_integers):
 
     if list_of_integers is None or list_of_integers == []:
         return None
-    low = 0
-    high = len(list_of_integers)
-    mid = ((high - low) // 2) + low
-    mid = int(mid)
-    if high == 1:
-        return list_of_integers[0]
-    if high == 2:
+    else:
         return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
