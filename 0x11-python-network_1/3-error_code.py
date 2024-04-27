932fdad875c0de4dd44934580a0ea52d69193a50
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-27 08:13:13 +0300

    Create 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,3 +12,7 @@ def main():
             print(response.read().decode('utf-8'))
     except error.HTTPError as e:
         print('Error code: {}'.format(e.code))
+
+
+if __name__ == "__main__":
+    main()
