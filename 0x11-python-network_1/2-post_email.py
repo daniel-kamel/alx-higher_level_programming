d8970c268758098a01b7ca455c4b3f7c1bd1c359
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-27 07:23:54 +0300

    Create 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ def main():
     """sends a POST request to the passed URL with the email as a parameter"""
     url = argv[1]
     value = {'email': argv[2]}
-    data = parse.urlencode(value).encode('ascii')
+    data = parse.urlencode(value).encode('utf-8')
     req = request.Request(url, data)
     with request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
