007e3cb98e58c710823ca174adcfb4987da6a84d
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-27 07:19:47 +0300

    Create 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Displays the body of the response and handles error codes"""
+from urllib import request, error
+from sys import argv
+
+
+def main():
+    """displays the body of the response"""
+    url = argv[1]
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
