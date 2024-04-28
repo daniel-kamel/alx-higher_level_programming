8949b606615b8ed29a144ac8578592dbd039ea71
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-04-28 18:44:58 +0300

    Create 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Uses GitHub's API to get the last 10 commits of a GitHub repository
+"""
+import requests
+from sys import argv
+
+
+def main():
+    """Uses GitHub's API to get the last 10 commits of a GitHub repository"""
+    repo = argv[1]
+    owner = argv[2]
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(owner, repo),
+                     params={'per_page': 10},
+                     headers={'Accept': 'application/vnd.github.v3+json'})
+    for commit in r.json()[:10]:
+        print(commit.get('sha'),
+              commit.get('commit').get('author').get('name'))
+
+
+if __name__ == "__main__":
+    main()
