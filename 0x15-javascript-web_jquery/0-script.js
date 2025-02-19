f7c41704aaf4eaf007c5bc04b0797b546040f793
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2025-02-20 00:36:16 +0200

    Add scripts to change header color using JavaScript and jQuery

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,4 @@
+// Using Javascript to change header color to red
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('header').style.color = '#FF0000';
 });
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,4 @@
+// Using jQuery to change header color to red
+$(document).ready(() => {
+  $('header').css('color', '#FF0000');
+});
